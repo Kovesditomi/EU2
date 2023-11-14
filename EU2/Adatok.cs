81b7944e7a5662e0894afc68e87f1a6ee445feb1
b19kövesdit <b19kvesdit@204-02>
2023-11-14 12:39:00 +0100

    Add project files.

---
+++ EU2/Adatok.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EU2
+{
+    internal class Adatok
+    {
+        public string Orszag { get; set; }
+        public string Fovaros{ get; set; }
+        public string Csatlakozas { get; set; }
+        public int Szavazat { get; set; }
+        public double Valtas { get; set; }
+        public string  Penznem{ get; set; }
+
+        public Adatok(string r)
+        {
+            string []v=r.Split(";");
+            Orszag = v[0];
+            Fovaros = v[1];
+            Csatlakozas = v[2];
+            Szavazat=int.Parse(v[3]);
+            Valtas = double.Parse(v[4]);
+            Penznem = v[5]; 
+        }
+        public override string ToString()
+        {
+            return $"Orszag:{Orszag}, Foőváros:{Fovaros},Csatlakozás:{Csatlakozas},Szavazat:{Szavazat},Váltás:{Valtas},Pénznem";
+
+        }
+    }
+    
+ 
+}
---
+++ EU2/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace EU2
+{
+    internal class Program
+    {
+
+
+
+        static List<Adatok> sorrend(List<Adatok> a)
+        { 
+        
+        var f1=a.Where(a => !a.Csatlakozas.Contains("Nem tag")).ToList();
+            var f1b = f1.OrderBy(x => x.Fovaros).ToList();
+            return f1b;
+        
+        }
+        static List<Adatok>Penz(List<Adatok> b)
+        {
+            var f2 = b.Where(b => b.Penznem.Contains("EUR")).ToList(); 
+            
+            return f2;
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+ 
+            var europa=new List<Adatok>();
+            using (var sr = new StreamReader(@"..\..\..\src\eu.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    europa.Add(new Adatok(sr.ReadLine()));
+                }
+            
+            }
+            foreach (var item in europa)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"1. feladat");
+            foreach (var item in sorrend(europa))
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"2. feladat");
+            foreach (var item in Penz(europa))
+            {
+                Console.WriteLine($"{item.Orszag} {item.Fovaros}");
+            }
+        }
+
+      
+    }
+}
